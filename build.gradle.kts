558724341801dc519601e11d80b6c364b987f472
FlummoxGamer <140507968+FlummoxGamer@users.noreply.github.com>
2025-11-03 17:48:48 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
     id("com.android.library") version "8.5.1" apply false
     kotlin("android") version "1.9.24" apply false
     
-    // CloudStream provider plugin
+    // CloudStream provider plugin (version now ignored due to settings.gradle.kts fix)
     id("com.lagradost.cloudstream3.provider") version "4.0.0" apply false
 }
-// This section defines repositories for dependencies (libraries used by the code).
-// Must be placed below the plugins block.
+
+// Define the repositories for *dependencies* (libraries used by the code).
 allprojects {
     repositories {
         google()
