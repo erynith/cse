1968ad211ccfd6790a7732e448d9e2c54174965b
FlummoxGamer <140507968+FlummoxGamer@users.noreply.github.com>
2025-11-03 18:51:10 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,18 @@
-plugins {
-    id("com.android.application") version "8.5.1" apply false
-    id("com.android.library") version "8.5.1" apply false
-    kotlin("android") version "1.9.24" apply false
-    id("com.lagradost.cloudstream3.provider") version "4.0.0" apply false
+buildscript {
+    repositories {
+        // CRITICAL: JitPack must be here to find the provider plugin artifact
+        maven { url = uri("https://jitpack.io") }
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Defines the provider plugin on the classpath using the correct JitPack coordinates
+        classpath("com.github.Lagradost:CloudStream-3:4.0.0")
+
+        // Standard build plugins
+        classpath("com.android.tools.build:gradle:8.5.1")
+        classpath(kotlin("gradle-plugin", version = "1.9.24"))
+    }
 }
 
 allprojects {
