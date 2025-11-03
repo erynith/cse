59ae30e9495315806fcce07f408bac43aaa2e620
FlummoxGamer <140507968+FlummoxGamer@users.noreply.github.com>
2025-11-04 01:32:52 +0530

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,6 +1,6 @@
-plugins {
-    id("com.lagacy.cloudstream.plugin") version "1.0.1"
-}
+// plugins {
+//     id("com.lagacy.cloudstream.plugin") version "1.0.1"
+// }
 
 dependencies {
     implementation("com.lagacy:ext-api:master-SNAPSHOT")
