82beff6990ef795363f8084c95e12e145bbfcac4
FlummoxGamer <140507968+FlummoxGamer@users.noreply.github.com>
2025-11-04 02:08:56 +0530

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -2,16 +2,6 @@ plugins {
     `java-library`
 }
 
-repositories {
-    maven {
-        url = uri("https://maven.pkg.github.com/recloudstream/cloudstream")
-        credentials {
-            username = System.getenv("GITHUB_ACTOR")
-            password = System.getenv("GITHUB_TOKEN")
-        }
-    }
-}
-
 dependencies {
-    implementation("com.github.recloudstream:cloudstream:master-SNAPSHOT")
+    implementation("com.lagacy:ext-api:master-SNAPSHOT")
 }
