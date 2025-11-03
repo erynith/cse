7d3dfcdda529c6cb0ca134749ecc5bbf635bbb28
FlummoxGamer <140507968+FlummoxGamer@users.noreply.github.com>
2025-11-03 16:51:56 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,5 @@
 // This file must be in the root directory.
 
-pluginManagement {
-    repositories {
-        // CRITICAL FIX: The repository where the *plugin* is located must be here.
-        maven("https://jitpack.io") 
-        google()
-        mavenCentral()
-    }
-}
-
 // Defines all modules in your project
 rootProject.name = "CloudstreamProvider"
 include(":ExampleProvider")
