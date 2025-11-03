021bd6c421fd7277e4dca8f2150c0128ebcdbfcc
FlummoxGamer <140507968+FlummoxGamer@users.noreply.github.com>
2025-11-03 17:08:15 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,16 @@
 // This file must be in the root directory.
 
+pluginManagement {
+    repositories {
+        // CRITICAL FIX: Google and Maven Central MUST be here to find the Android plugins (com.android.application)
+        google()
+        mavenCentral()
+        // The CloudStream provider plugin is also found on JitPack, include it here
+        maven("https://jitpack.io")
+        gradlePluginPortal()
+    }
+}
+
 // Defines all modules in your project
 rootProject.name = "CloudstreamProvider"
 include(":ExampleProvider")
