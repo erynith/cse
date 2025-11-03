1637e1574f88c926326a907bf5cc30b40f4258d1
FlummoxGamer <140507968+FlummoxGamer@users.noreply.github.com>
2025-11-03 18:01:40 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,17 @@
-// This file must be in the root directory.
-
 pluginManagement {
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
-// CRITICAL FIX: Include the provider plugin repository source directly from GitHub
-includeBuild("https://github.com/recloudstream/Cloudstream3") {
-    dependencySubstitution {
-        // Substitute the official plugin ID with the source code from the included build
-        substitute(module("com.lagradost.cloudstream3:provider")).using(project(":provider"))
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
     }
 }
 
