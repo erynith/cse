569d25173ed0bc4417a2e57cba8e4f9b8942f01a
erynith <135774005+erynith@users.noreply.github.com>
2026-02-16 13:14:16 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ subprojects {
 
         tasks.withType<KotlinJvmCompile> {
             compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_1_8) // Required
+                jvmTarget.set(JvmTarget.JVM_1_8)
                 freeCompilerArgs.addAll(
                     "-Xno-call-assertions",
                     "-Xno-param-assertions",
