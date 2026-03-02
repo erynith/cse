6f1482574d7cdc5194f3c6f0cdf95b8339da60ff
erynith <135774005+erynith@users.noreply.github.com>
2026-03-01 21:33:25 -0500

    test

--- DebridStreamEx/src/main/kotlin/com/erynith/DebridStreamEx.kt
+++ DebridStreamEx/src/main/kotlin/com/erynith/DebridStreamEx.kt
@@ -291,11 +291,11 @@ class DebridStreamEx(private val sharedPref: SharedPreferences) : TmdbProvider()
         val shared = mutableSetOf<String>()
         runAllAsync(
             suspend { invokeTorrentio(service, key, res.imdbId, res.season, res.episode, shared, subtitleCallback, callback) },
-            suspend { invokeComet(service, key, res.imdbId, res.season, res.episode, shared, subtitleCallback, callback) },
+            //suspend { invokeComet(service, key, res.imdbId, res.season, res.episode, shared, subtitleCallback, callback) },
             suspend { invokeStremthru(service, key, res.imdbId, res.season, res.episode, shared, subtitleCallback, callback) },
             //suspend { invokeMeteor(service, key, res.imdbId, res.season, res.episode, shared, subtitleCallback, callback) },
             //suspend { invokeWatchsomuch(res.imdbId, res.season, res.episode, subtitleCallback) },
-            //suspend { invokeOpenSubsPro(res.imdbId, res.season, res.episode, subtitleCallback) },
+            suspend { invokeOpenSubsPro(res.imdbId, res.season, res.episode, subtitleCallback) },
             suspend { invokeOpenSubs(res.imdbId, res.season, res.episode, subtitleCallback) }
         )
 
